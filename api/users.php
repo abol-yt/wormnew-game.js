c3320e2f278444daa53b598ab13ae5ed3575fc5d
abol-yt <soltan.amir.soltan.1383@gmail.com>
2025-07-16 17:40:55 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -343,6 +343,25 @@
               "Client_VisibleSkin8": "0000",
               "Client_VisibleSkin9": "0000",
               "Client_VisibleSkin10": "0000"
+        },
+        {
+              "id": 20,
+              "cliente_NOMBRE": "abod",
+              "cliente_ID": "gg_103647891929502707022",
+              "Client_KeyAccecs": "XTPRIVATESKIN",
+              "cliente_DateExpired": "2026-06-17",
+              "status": 1,
+              "Client_VisibleSkin": "0000",
+              "Client_VisibleSkin1": "0000",
+              "Client_VisibleSkin2": "0000",
+              "Client_VisibleSkin3": "0000",
+              "Client_VisibleSkin4": "0000",
+              "Client_VisibleSkin5": "0000",
+              "Client_VisibleSkin6": "0000",
+              "Client_VisibleSkin7": "0000",
+              "Client_VisibleSkin8": "0000",
+              "Client_VisibleSkin9": "0000",
+              "Client_VisibleSkin10": "0000"
         }
         
         
